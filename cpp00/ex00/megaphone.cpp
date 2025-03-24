5f7168f2b86d801c5c50cd2f4f67d807887bc785
Beyza Arslan <121162425+byzarsln@users.noreply.github.com>
2025-03-24 18:42:28 +0300

    dnmne

--- cpp00/ex00/megaphone.cpp
+++ cpp00/ex00/megaphone.cpp
@@ -12,7 +12,7 @@ int main(int argc, char **argv)
         std::cout << std::endl;
    }
     else if (argc == 1)
-        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOI SE *" << std::endl;
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
     return 0;
 }
 
