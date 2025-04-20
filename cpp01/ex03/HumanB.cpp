4782c49135058ec23988dddad7fb32818d8f1784
Beyza Arslan <121162425+byzarsln@users.noreply.github.com>
2025-04-20 14:14:11 +0300

    null changed

--- cpp01/ex03/HumanB.cpp
+++ cpp01/ex03/HumanB.cpp
@@ -1,6 +1,6 @@
 #include "HumanB.hpp"
 
-HumanB::HumanB(std::string name) : name(name), weapon(nullptr)
+HumanB::HumanB(std::string name) : name(name), weapon(NULL)
 {
     std::cout << "HumanB " << this->name << " has been created" << std::endl;
 }
